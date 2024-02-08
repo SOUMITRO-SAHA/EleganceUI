4573db6555ec32cc224d6e3532844427d1aa1283
SOUMITRA SAHA <soumitrosahaofficial@gmail.com>
2024-02-08 20:15:14 +0530

    Storybook basic setup complete

---
+++ apps/docs/.storybook/YourTheme.js
@@ -0,0 +1,12 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  colorPrimary: "#111827",
+  colorSecondary: "#111827",
+  appContentBg: "#F9FAFC",
+  brandTitle: "Elegance UI",
+  brandUrl: "",
+  brandImage: "",
+  brandTarget: "_self",
+});
--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
